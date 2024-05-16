2e143b41d06b516ef04ba058a6c11e5b4821642f
FranjeGueje <fjgj1@hotmail.com>
2024-05-16 21:29:41 +0200

    Change of methon for engine

--- rascally/engines/epic.py
+++ rascally/engines/epic.py
@@ -1,10 +1,10 @@
-# 
+#
 # Copyright (C) 2024 Paco Guerrero <fjgj1@hotmail.com>
-# 
+#
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 #############################################################################################
 # Rascally
@@ -24,17 +24,17 @@ class EngineEpic(LauncherEngine):
 
     def __init__(self):
         super().__init__('Epic')
-        
+
         #! Añandimos los directorios, ficheros, y enlaces EXTRAS
         self.empty_dir_list = self.empty_dir_list + \
             ('pfx/drive_c/users/steamuser/AppData/Local/EpicGamesLauncher/Intermediate/Config/CoalescedSourceConfigs',)
-            
+
         self.copy_list = self.copy_list + \
             ('pfx/drive_c/ProgramData/Epic',
              'pfx/drive_c/users/steamuser/AppData/Local/Epic Games',
              'pfx/drive_c/users/steamuser/AppData/Local/EpicGamesLauncher/Intermediate/Config/CoalescedSourceConfigs/PortalRegions.ini'
              )
-        
+
         self.symlink_list = self.symlink_list + \
             ('pfx/drive_c/users/steamuser/AppData/Local/EpicGamesLauncher/Saved',)
 
@@ -53,32 +53,45 @@ class EngineEpic(LauncherEngine):
 
         # Iterar sobre cada carpeta y realizar la acción deseada
         for carpeta in carpetas:
-            path_exe = os.path.join(
-                path, carpeta + "/pfx/drive_c/Program Files (x86)/Epic Games/Launcher/Engine/Binaries/Win64/EpicGamesLauncher.exe")
             path_exclude = os.path.join(path, carpeta + "/rascal_exclude")
-            if os.path.isfile(path_exe) and not os.path.isfile(path_exclude):
-                logger.info(
-                    f'[EngineEpic]Found this prefix {path}/{carpeta} with Epic Games Launcher installed')
-                path_json = os.path.join(
-                    path, carpeta + "/pfx/drive_c/ProgramData/Epic/UnrealEngineLauncher/LauncherInstalled.dat")
-                if os.path.isfile(path_json):
-                    # Abrir el archivo JSON en modo lectura
-                    with open(path_json, 'r') as archivo:
-                        # Cargar el contenido del archivo en una variable Python
-                        data = json.load(archivo)
+            if os.path.isfile(path_exclude):
+                continue
 
-                    for jueguecito in data['InstallationList']:
-                        title = os.path.basename(jueguecito['InstallLocation'])
-                        exe = jueguecito['AppName']
-                        exe_completed = f'"com.epicgames.launcher://apps/{exe}?action=launch&silent=true"'
-                        logger.info(
-                            f'[EngineEpic]Found the game {title} with Exe {exe_completed}')
-                        g = BasicGame(
-                            AppName=title,
-                            Exe=exe_completed,
-                            CompatData=f'{path}{carpeta}'
-                        )
-                        juegos.append(g)
+            path_reg = os.path.join(path, carpeta + "/pfx/system.reg")
+            exe_engine = 'EpicGamesLauncher.exe,0'.lower()
+
+            if os.path.isfile(path_reg):
+                try:
+                    with open(path_reg, 'r') as f:
+                        for linea in f:
+                            if exe_engine in linea.lower():
+                                logger.info(
+                                    f'[EngineEpic]Found this prefix {path}/{carpeta} with Epic Games Launcher installed')
+                                path_json = os.path.join(
+                                    path, carpeta + "/pfx/drive_c/ProgramData/Epic/UnrealEngineLauncher/LauncherInstalled.dat")
+                                if os.path.isfile(path_json):
+                                    # Abrir el archivo JSON en modo lectura
+                                    with open(path_json, 'r') as archivo:
+                                        # Cargar el contenido del archivo en una variable Python
+                                        data = json.load(archivo)
+
+                                    for jueguecito in data['InstallationList']:
+                                        title = os.path.basename(
+                                            jueguecito['InstallLocation'])
+                                        exe = jueguecito['AppName']
+                                        exe_completed = f'"com.epicgames.launcher://apps/{exe}?action=launch&silent=true"'
+                                        logger.info(
+                                            f'[EngineEpic]Found the game {title} with Exe {exe_completed}')
+                                        g = BasicGame(
+                                            AppName=title,
+                                            Exe=exe_completed,
+                                            CompatData=f'{path}{carpeta}'
+                                        )
+                                        juegos.append(g)
+                                break
+                    continue
+                except:
+                    continue
 
         logger.info(f'[EngineEpic]Found {len(juegos)} games')
         return tuple(juegos)
--- rascally/engines/ubi.py
+++ rascally/engines/ubi.py
@@ -1,10 +1,10 @@
-# 
+#
 # Copyright (C) 2024 Paco Guerrero <fjgj1@hotmail.com>
-# 
+#
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 #############################################################################################
 # Rascally
@@ -18,20 +18,21 @@ from . import LauncherEngine
 from . import BasicGame
 from util import logger
 
+
 class EngineUbisoft(LauncherEngine):
-    
+
     def __init__(self):
         super().__init__('Ubisoft')
-        
+
         #! Añandimos los directorios, ficheros, y enlaces EXTRAS
         self.copy_list = self.copy_list + \
             ('pfx/drive_c/users/steamuser/AppData/Local/Ubisoft Game Launcher',)
-        
-    def search_games(self, search_path=os.environ.get("HOME") + "/.local/share/Steam", excep={})-> Tuple[BasicGame]:
-    
+
+    def search_games(self, search_path=os.environ.get("HOME") + "/.local/share/Steam", excep={}) -> Tuple[BasicGame]:
+
         # Establecemos la ruta donde están los compatdata
         path = search_path + "/steamapps/compatdata/"
-        
+
         # Obtener la lista de carpetas en la ruta
         carpetas = [nombre for nombre in os.listdir(
             path) if os.path.isdir(os.path.join(path, nombre))]
@@ -42,30 +43,44 @@ class EngineUbisoft(LauncherEngine):
 
         # Iterar sobre cada carpeta y realizar la acción deseada
         for carpeta in carpetas:
-            ruta_ubi = os.path.join(
-                path, carpeta + "/pfx/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/UbisoftConnect.exe")
             path_exclude = os.path.join(path, carpeta + "/rascal_exclude")
-            if os.path.isfile(ruta_ubi) and not os.path.isfile(path_exclude):
-                prefix = path + carpeta
-                
-                logger.info(f'[EngineUbisoft]Found this prefix {prefix} with Ubisoft installed')
+            if os.path.isfile(path_exclude):
+                continue
 
-                with open(os.path.join(path, carpeta + "/pfx/system.reg"), 'r') as reg_file:
-                    lines = reg_file.read()
-                
-                juegos_bruto = lines.split('[Software\\\\Wow6432Node\\\\Ubisoft\\\\Launcher\\\\Installs\\\\')
-                
-                for i in range(1, len(juegos_bruto)):
-                    exe = juegos_bruto[i].split(']')[0]
-                    exe_completed = "\"uplay://launch/" + exe + "/0\""
-                    install_dir = juegos_bruto[i].split("\"")[3]
-                    title = os.path.basename(install_dir[1:-1])
-                    logger.info(f'[EngineUbisoft]Found the game {title} with Exe {exe_completed}')
-                    g = BasicGame(
-                        AppName=title,
-                        Exe=exe_completed,
-                        CompatData=prefix
-                    )
-                    juegos.append(g)
+            path_reg = os.path.join(path, carpeta + "/pfx/system.reg")
+            exe_engine = 'UbisoftConnect.exe'.lower()
+
+            if os.path.isfile(path_reg):
+                try:
+                    with open(path_reg, 'r') as f:
+                        for linea in f:
+                            if exe_engine in linea.lower():
+                                logger.info(
+                                    f'[EngineEpic]Found this prefix {path}/{carpeta} with Epic Games Launcher installed')
+
+                                with open(os.path.join(path, carpeta + "/pfx/system.reg"), 'r') as reg_file:
+                                    lines = reg_file.read()
+
+                                juegos_bruto = lines.split(
+                                    '[Software\\\\Wow6432Node\\\\Ubisoft\\\\Launcher\\\\Installs\\\\')
+
+                                for i in range(1, len(juegos_bruto)):
+                                    exe = juegos_bruto[i].split(']')[0]
+                                    exe_completed = "\"uplay://launch/" + exe + "/0\""
+                                    install_dir = juegos_bruto[i].split("\"")[
+                                        3]
+                                    title = os.path.basename(install_dir[1:-1])
+                                    logger.info(
+                                        f'[EngineUbisoft]Found the game {title} with Exe {exe_completed}')
+                                    g = BasicGame(
+                                        AppName=title,
+                                        Exe=exe_completed,
+                                        CompatData=path + carpeta
+                                    )
+                                    juegos.append(g)
+                                break
+                    continue
+                except:
+                    continue
         logger.info(f'[EngineUbisoft]Found {len(juegos)} games')
         return tuple(juegos)
